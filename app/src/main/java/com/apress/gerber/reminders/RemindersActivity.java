16dcf45998ef21b32de92e9ad8e06c35c3f25bfd
Adam Gerber <adam.gerber@yahoo.com>
2015-05-01 18:34:00 -0500

    Adds ListView with custom colors

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -4,14 +4,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class RemindersActivity extends ActionBarActivity {
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
+        //The arrayAdatper is the controller in our
+        //model-view-controller relationship. (controller)
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                //context
+                this,
+                //layout (view)
+                R.layout.reminders_row,
+                //row (view)
+                R.id.row_text,
+                //data (model) with bogus data to test our listview
+                new String[]{"first record", "second record", "third record"});
+        mListView.setAdapter(arrayAdapter);
     }
 
 
