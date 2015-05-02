b73d1d73d0dfe2547578494ad3598fab9991720d
Adam Gerber <adam.gerber@yahoo.com>
2015-05-02 12:08:52 -0500

    Adds database Create, Read, Update, and Delete support with custom dialogs

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -13,10 +13,16 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -66,6 +72,38 @@ public class RemindersActivity extends ActionBarActivity {
         //with data from the db(model)
         mListView.setAdapter(mCursorAdapter);
 
+        //when we click an individual item in the listview
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int masterListPosition, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(RemindersActivity.this);
+                ListView modeListView = new ListView(RemindersActivity.this);
+                String[] modes = new String[]{"Edit Reminder", "Delete Reminder"};
+                ArrayAdapter<String> modeAdapter = new ArrayAdapter<>(RemindersActivity.this,
+                        android.R.layout.simple_list_item_1, android.R.id.text1, modes);
+                modeListView.setAdapter(modeAdapter);
+                builder.setView(modeListView);
+                final Dialog dialog = builder.create();
+                dialog.show();
+                modeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        //edit reminder
+                        if (position == 0) {
+                            int nId = getIdFromPosition(masterListPosition);
+                            Reminder reminder = mDbAdapter.fetchReminderById(nId);
+                            fireCustomDialog(reminder);
+                            //delete reminder
+                        } else {
+                            mDbAdapter.deleteReminderById(getIdFromPosition(masterListPosition));
+                            mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
+                        }
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
             mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
@@ -110,7 +148,7 @@ public class RemindersActivity extends ActionBarActivity {
     }
 
     private int getIdFromPosition(int nC) {
-        return (int)mCursorAdapter.getItemId(nC);
+        return (int) mCursorAdapter.getItemId(nC);
     }
 
     private void insertSomeReminders() {
@@ -130,37 +168,51 @@ public class RemindersActivity extends ActionBarActivity {
         mDbAdapter.createReminder("Buy 300,000 shares of Google", false);
         mDbAdapter.createReminder("Call the Dalai Lama back", true);
 
-        //when we click an individual item in the listview
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+    }
+
+    private void fireCustomDialog(final Reminder reminder) {
+        // custom dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_custom);
+        TextView titleView = (TextView) dialog.findViewById(R.id.custom_title);
+        final EditText editCustom = (EditText) dialog.findViewById(R.id.custom_edit_reminder);
+        Button commitButton = (Button) dialog.findViewById(R.id.custom_button_commit);
+        final CheckBox checkBox = (CheckBox) dialog.findViewById(R.id.custom_check_box);
+        LinearLayout rootLayout = (LinearLayout) dialog.findViewById(R.id.custom_root_layout);
+        final boolean isEditOperation = (reminder != null);
+        //this is for an edit
+        if (isEditOperation) {
+            titleView.setText("Edit Reminder");
+            checkBox.setChecked(reminder.getImportant() == 1);
+            editCustom.setText(reminder.getContent());
+            rootLayout.setBackgroundColor(getResources().getColor(R.color.blue));
+        }
+        commitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, final int masterListPosition, long id) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(RemindersActivity.this);
-                ListView modeListView = new ListView(RemindersActivity.this);
-                String[] modes = new String[] { "Edit Reminder", "Delete Reminder" };
-                ArrayAdapter<String> modeAdapter = new ArrayAdapter<>(RemindersActivity.this,
-                        android.R.layout.simple_list_item_1, android.R.id.text1, modes);
-                modeListView.setAdapter(modeAdapter);
-                builder.setView(modeListView);
-                final Dialog dialog = builder.create();
-                dialog.show();
-                modeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        //edit reminder
-                        if (position == 0) {
-                            Toast.makeText(RemindersActivity.this, "edit "
-                                    + masterListPosition, Toast.LENGTH_SHORT).show();
-                        //delete reminder
-                        } else {
-                            Toast.makeText(RemindersActivity.this, "delete "
-                                    + masterListPosition, Toast.LENGTH_SHORT).show();
-                        }
-                        dialog.dismiss();
-                    }
-                });
+            public void onClick(View v) {
+                String reminderText = editCustom.getText().toString();
+                if (isEditOperation) {
+                    Reminder reminderEdited = new Reminder(reminder.getId(),
+                            reminderText, checkBox.isChecked() ? 1 : 0);
+                    mDbAdapter.updateReminder(reminderEdited);
+                    //this is for new reminder
+                } else {
+                    mDbAdapter.createReminder(reminderText, checkBox.isChecked());
+                }
+                mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
+                dialog.dismiss();
             }
         });
-
+        Button buttonCancel = (Button) dialog.findViewById(R.id.custom_button_cancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 
     @Override
@@ -172,13 +224,10 @@ public class RemindersActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
             case R.id.action_new:
                 //create new Reminder
-                Log.d(getLocalClassName(), "create new Reminder");
+                fireCustomDialog(null);
                 return true;
             case R.id.action_exit:
                 finish();
