006beb87ac3582d4bcee599411ca7943d30636b2
Adam Gerber <adam.gerber@yahoo.com>
2015-05-02 11:11:30 -0500

    Adds Contextual Action Mode with context action menu

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,14 +1,19 @@
 package com.apress.gerber.reminders;
 
+import android.annotation.TargetApi;
 import android.app.Dialog;
 import android.database.Cursor;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -20,6 +25,7 @@ public class RemindersActivity extends ActionBarActivity {
     private RemindersDbAdapter mDbAdapter;
     private RemindersSimpleCursorAdapter mCursorAdapter;
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +35,7 @@ public class RemindersActivity extends ActionBarActivity {
         mDbAdapter = new RemindersDbAdapter(this);
         mDbAdapter.open();
         if (savedInstanceState == null) {
-           //Clear all data
+            //Clear all data
             mDbAdapter.deleteAllReminders();
             //Add some data
             insertSomeReminders();
@@ -59,6 +65,52 @@ public class RemindersActivity extends ActionBarActivity {
         //the cursorAdapter (controller) is now updating the listView (view)
         //with data from the db(model)
         mListView.setAdapter(mCursorAdapter);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+            mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+                @Override
+                public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean
+                        checked) {
+                }
+
+                @Override
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    MenuInflater inflater = mode.getMenuInflater();
+                    inflater.inflate(R.menu.cam_menu, menu);
+                    return true;
+                }
+
+                @Override
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                @Override
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.menu_item_delete_reminder:
+                            for (int nC = mCursorAdapter.getCount() - 1; nC >= 0; nC--) {
+                                if (mListView.isItemChecked(nC)) {
+                                    mDbAdapter.deleteReminderById(getIdFromPosition(nC));
+                                }
+                            }
+                            mode.finish();
+                            mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
+                            return true;
+                    }
+                    return false;
+                }
+
+                @Override
+                public void onDestroyActionMode(ActionMode mode) {
+                }
+            });
+        }
+    }
+
+    private int getIdFromPosition(int nC) {
+        return (int)mCursorAdapter.getItemId(nC);
     }
 
     private void insertSomeReminders() {
